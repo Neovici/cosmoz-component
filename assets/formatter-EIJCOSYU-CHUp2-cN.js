e8f6f38b797551eae0afae6e87ecbeb9a2adbc2d
cristinecula <VirusZ@gmail.com>
2026-02-04 09:27:25 +0000

    Deploying to gh-pages from @ Neovici/cosmoz-component@3af42ccba5e9b9cf0712fbeac1c8ddf0a8801b3d 🚀

--- assets/formatter-EIJCOSYU-CHUp2-cN.js
+++
@@ -1,1 +0,0 @@
-import{d as i,b as m}from"./DocsRenderer-LL677BLK-Ccclthjd.js";import{I as a}from"./iframe-CUw33J6m.js";var o=a(m(),1),d=(0,o.default)(2)(async(e,r)=>e===!1?r:i(r));export{d as formatter};
